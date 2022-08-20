540be40b18cba60ad0b7dbca1297d3f19be0e830
drewrvaz <drewrvaz@gmail.com>
2022-08-20 18:45:52 -0400

    Added functions to retrieve the weather data and save the city data. They do not work yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,35 @@
 var APIKey = "3ea4d2de1606fec8c25abcf7659af663";
-var city;
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
+cityList = [];
 
-fetch(queryURL)
-.then(function (response) {
-  if (response.ok) {
-    console.log(response);
-    response.json().then(function (data) {
-      console.log(data);
-    });
-  } });
+$(document).ready(function() {
+  var pastCities = JSON.parse(localStorage.getItem(cityList));
+
+  if (cityList !== null) {
+    cityList = pastCities;
+  }
+})
+
+$("#searchBtn").on("click", function(event) {
+  event.preventDefault();
+  var city = $("#search-city").val();
+  if (city === "") {
+     return(city);
+  }
+  cityList.push(city);
+  console.log("clicked!")
+
+  getWeather();
+  savedCities();
+})
+
+function getWeather() {
+  currentCity = $("#search-city")
+  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
+  fetch(queryURL)
+  .then(function (response) {
+    return response.json();
+})
+  .then(function(response) {
+    console.log(currentCity)
+  })
+}
