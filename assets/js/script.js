8ae72f863be0b92b02dafec8d60645b6d4ed7148
drewrvaz <drewrvaz@gmail.com>
2022-08-20 18:02:56 -0400

    Played with the API link to make sure that it is working properly when a city is input. Also added a span tag to the HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,10 @@ var city;
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
 
 fetch(queryURL)
+.then(function (response) {
+  if (response.ok) {
+    console.log(response);
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  } });
