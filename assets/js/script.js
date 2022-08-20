2942e323c7c0cfc8652a1dad3109c069ea24f67c
drewrvaz <drewrvaz@gmail.com>
2022-08-20 19:37:33 -0400

    Changed a couple names in the html. Trying to troubleshoot two different ways to save city data in localstorage. Neither one works right now. Yay!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,54 @@
 var APIKey = "3ea4d2de1606fec8c25abcf7659af663";
-cityList = [];
+var searchBtn = $("#searchBtn");
+var cityInput = $("#city-input");
+var listOfCities = $("#list-of-cities");
+var cityForm = $("#city-form")
 
-$(document).ready(function() {
-  var pastCities = JSON.parse(localStorage.getItem(cityList));
+// $(document).ready(function() {
+//   var pastCities = JSON.parse(localStorage.getItem(cityList));
 
-  if (cityList !== null) {
-    cityList = pastCities;
-  }
-})
+//   if (cityList !== null) {
+//     cityList = pastCities;
+//   }
+// })
+
+let cityStorage = localStorage.getItem("search-city")
+  ? JSON.parse(localStorage.getItem("search-city"))
+  : [];
 
 $("#searchBtn").on("submit", function(event) {
   event.preventDefault();
-  var city = $("#search-city").val();
-  if (city === "") {
-     return(city);
-  }
-  cityList.push(city);
-  console.log("clicked!")
+  cityStorage.push(cityInput.value);
+  localStorage.setItem("search-city", JSON.stringify(cityStorage));
+  listBuilder(cityInput.value);
+  cityInput.value = "";
+});
 
-  getWeather();
-  savedCities();
-})
+var listBuilder = (text) => {
+  var pastCity = document.createElement("li");
+  pastCity.innerHTML = text;
+  notes.appendChild(pastCity);
+}
+
+var getCities = JSON.parse(localStorage.getItem("search-city"));
+getCities.forEach(pastCity) {
+  listBuilder(pastCity)
+}
+
+// $("#searchBtn").on("submit", function(event) {
+//   event.preventDefault();
+//   var city = $("#search-city").val();
+//   if (city === "") {
+//      return(city);
+//   }
+//   cityList.push(city);
+//   console.log("clicked!")
+
+//   getWeather();
+//   savedCities();
+// })
+
+// console.log(cityList)
 
 function getWeather() {
   currentCity = $("#search-city")
