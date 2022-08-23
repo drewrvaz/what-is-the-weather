ff9994fb1da5870e950000f7cae2f48a94fc282b
drewrvaz <drewrvaz@gmail.com>
2022-08-23 13:37:22 -0400

    I figured out how to get my future forecast to show! Sadly my UV Index is still broken. Working on that one now

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,7 +43,7 @@ function presentCity() {
 };
 
 function getWeather() {
-  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + APIKey;
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + APIKey;
   fetch(queryURL)
   .then(function(response) {
     return response.json();
@@ -59,30 +59,75 @@ function getWeather() {
     latitude = data.coord.lat
     longitude = data.coord.lon
 
-    var uvAPIurl = "http://api.openweather.org/data/2.5/uvi?&lat=" + latitude + "&lon" + longitude + "&APPID=" + APIKey;
-    fetch(uvAPIurl)
+    // var uvAPIurl = "http://api.openweather.org/data/2.5/uvi?lat=" + latitude + "&lon" + longitude + "&APPID=" + APIKey;
+    // fetch(uvAPIurl)
 
-    .then(function(response) {
-      return response.json();
-    })
+    // .then(function(response) {
+    //   return response.json();
+    // })
 
-    .then(function(data) {
-      uvIndex = data.value;
+    // .then(function(data) {
+    //   var uvIndex = data.value;
 
-      if(uvIndex <= 2) {
-          $("#todaysWeather").addClass("low")
-      } else if(2 < uvIndex <= 5) {
-        $("#todaysWeather").addClass("moderate")
-      } else if(5 < uvIndex <= 7) {
-        $("#todaysWeather").addClass("high")
-      } else if(uvIndex >= 8) {
-        $("#todaysWeather").addClass("extreme")
-      }
-    })
+    //   if(uvIndex <= 2) {
+    //       $("#todaysWeather").addClass("low")
+    //   } else if(2 < uvIndex <= 5) {
+    //     $("#todaysWeather").addClass("moderate")
+    //   } else if(5 < uvIndex <= 7) {
+    //     $("#todaysWeather").addClass("high")
+    //   } else if(uvIndex >= 8) {
+    //     $("#todaysWeather").addClass("extreme")
+    //   }
+    // })
 
     currentWeather();
-    showFutureCast();
+    showFutureCast(city);
   })
+}
+
+function showFutureCast() {
+  // city = $("#city-input").val();
+
+  var futureCastAPI = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey + "&units=imperial";
+  fetch(futureCastAPI)
+  .then(function(response) {
+    return response.json();
+  })
+
+  .then(function(data) {
+    city = $("#city-input").val();
+    var futureCast = data.list;
+
+    for (let i = 0; i < 5; i++) {
+      futureDate = moment().add(i, "days").format("MM/DD/YYYY");
+      futureWeatherIcon = data.list[i].weather[0].icon;
+      futureWeatherIconEL = "http://openweathermap.org/img/w/" + futureWeatherIcon + ".png";
+      futureHumidity = futureCast[i].main.humidity;
+      futureTemp = futureCast[i].main.temp;
+      futureWindSpeed = futureCast[i].wind.speed
+
+      var card = $("<div class='card'>").addClass("futureCastCards");
+      var cardDiv = $("<div>").attr("class", "card-block")
+      var cardDate = $("<h6>").text(futureDate).addClass("pt-2");
+      var dateDiv = $("<div>").attr("class", "card-block")
+      var textDiv = $("<div>").attr("class", "card-text")
+      var imgEl = $("<img>").attr("src", futureWeatherIconEL);
+      var futureTempEl = $("<p>").text("Temp: " + futureTemp + " ºF")
+      var futureWindEl = $("<p>").text("Wind: " + futureWindSpeed + " MPH")
+      var futureHumidityEl = $("<p>").text("Humidity " + futureHumidity + "%");
+
+      dateDiv.append(cardDate);
+      cardDiv.append(dateDiv);
+      textDiv.append(imgEl);
+      textDiv.append(futureTempEl);
+      textDiv.append(futureWindEl);
+      textDiv.append(futureHumidityEl);
+      card.append(cardDiv);
+      cardDiv.append(textDiv);
+      $(".card-deck").append(card);
+    }
+  })
+  console.log(futureCastAPI)
 }
 
 function currentWeather() {
@@ -120,53 +165,9 @@ function currentWeather() {
   $("#todaysWeather").append(weatherDiv);
   console.log(windSpeedEl);
 
-  var uvEl = $("<div>").text(uvIndex);
-  weatherDiv.append(uvEl);
-  $("#todaysWeather").append(weatherDiv);
-  console.log(uvEl);
+  // var uvEl = $("<div>").text(uvIndex);
+  // weatherDiv.append(uvEl);
+  // $("#todaysWeather").append(weatherDiv);
+  // console.log(uvEl);
 }
 
-function showFutureCast() {
-  city = $("#city-input").val();
-
-  var futureCastAPI = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + "&AAPID=" + APIKey;
-  fetch(futureCastAPI)
-  .then(function(response) {
-    return response.json();
-  })
-
-  .then(function(data) {
-    city = $("#city-input").val();
-    var futureCast = data.list;
-
-    for (let i = 0; i < 5; i++) {
-      futureDate = moment.add(i, "days").format("MM/DD/YYYY");
-      futureWeatherIcon = data.list[i].weather[0].icon;
-      futureWeatherIconEL = "http://openweathermap.org/img/w/" + futureWeatherIcon + ".png";
-      futureHumidity = futureCast[i].main.humidity;
-      futureTemp = futureCast[i].main.temp;
-      futureWindSpeed = futureCast[i].wind.speed
-    }
-
-    var card = $("<div class='card'>").addClass("futureCastCards");
-    var cardDiv = $("<div>").attr("class", "card-block")
-    var cardDate = $("<h6>").text(futureDate).addClass("pt-2");
-    var dateDiv = $("<div>").attr("class", "card-block")
-    var textDiv = $("<div>").attr("class", "card-text")
-    var imgEl = $("<img>").attr("src", futureWeatherIconEL);
-    var futureTempEl = $("<p>").text("Temp: " + futureTemp + " ºF")
-    var futureWindEl = $("<p>").text("Wind: " + futureWindSpeed + " MPH")
-    var futureHumidityEl = $("<p>").text("Humidity " + futureHumidity + "%");
-
-    dateDiv.append(cardDate);
-    cardDiv.append(dateDiv);
-    textDiv.append(imgEl);
-    textDiv.append(futureTempEl);
-    textDiv.append(futureWindEl);
-    textDiv.append(futureHumidityEl);
-    card.append(cardDiv);
-    cardDiv.append(textDiv);
-    $(".card-deck").append(card);
-  })
-  console.log(futureCastAPI)
-}
